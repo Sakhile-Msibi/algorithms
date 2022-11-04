aece85220f4b72261b191c95a262a0f838321fa2
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-11-04 17:46:45 +0200

    Added HashSet Example

---
+++ HashSetExample/HashSetExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> myhash1 = new HashSet<string>()
+            {
+                "C", "C++", "C#", "Java", "Ruby"
+            };
+
+            HashSet<string> myhash2 = new HashSet<string>()
+            {
+                "PHP", "C++", "Perl", "Java"
+            };
+
+            myhash2.IntersectWith(myhash1);
+            Console.WriteLine("myhash1\n");
+            foreach (var elem in myhash1)
+                Console.Write(elem + " ");
+
+            Console.WriteLine("\nmyhash2\n");
+            foreach (var elem in myhash2)
+                Console.Write(elem + " ");
+        }
+    }
+}
